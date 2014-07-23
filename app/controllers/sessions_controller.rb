83387962760bfddb2a84d275e5c575c4a6b0289f
Karan Purohit <karan@Karan.(none)>
2014-07-23 17:30:32 +0530

    Initial commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,21 @@
 class SessionsController < ApplicationController
 
   def new
-    @title = "Sign in"
   end
-  
+
   def create
-    user = User.authenticate(params[:session][:email],
-                             params[:session][:password])
-    if user.nil?
-      flash.now[:error] = "Invalid email/password combination."
-      @title = "Sign in"
-      render 'new'
-    else
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_back_or user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
     end
   end
-  
+
   def destroy
     sign_out
-    redirect_to root_path
+    redirect_to root_url
   end
 end
